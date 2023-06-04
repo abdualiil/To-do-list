3bb7d383dc2b25cd5a5601aa75341cea58921fb4
abdualiil <133022687+abdualiil@users.noreply.github.com>
2023-06-04 14:43:00 +0600

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+import {Filter} from './Components/Filter';
+import {Footer} from './Components/Footer';
+import {Header} from './Components/Header';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Filter />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
