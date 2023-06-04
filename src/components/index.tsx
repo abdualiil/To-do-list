fa2608981ad4e2e4b3b75e01c3100c033865ea32
abdualiil <133022687+abdualiil@users.noreply.github.com>
2023-06-04 15:06:08 +0600

    Create index.tsx

---
+++ src/components/index.tsx
@@ -0,0 +1,31 @@
+import { memo, useState } from 'react';
+import './AddTask.css';
+import {AddTaskModal} from './AddTaskModal';
+type ModalProps = {
+	textOfTask: any,
+	createTask: any,
+	taskName: string,
+	setTaskName: any,
+	sumbitOnEnter: any,
+	openModal: any,
+	addIsClicked: boolean
+}
+export const AddTask:React.FC<ModalProps> = memo((props) => {
+	const {textOfTask, createTask, taskName, setTaskName, sumbitOnEnter, openModal, addIsClicked} = props;
+
+  	return (
+		<>
+			<button onClick = {openModal} className={addIsClicked ? "addTask addTask_active" : "addTask"}>
+				<div className="addTask__verticalLine"></div>
+				<div className="addTask__horizontalLine"></div>
+			</button>
+			{addIsClicked && <AddTaskModal 
+								textOfTask = {textOfTask} 
+								createTask={createTask} 
+								taskName={taskName} 
+								setTaskName={setTaskName} 
+								sumbitOnEnter={sumbitOnEnter}
+							/>}
+		</>
+	)
+})
